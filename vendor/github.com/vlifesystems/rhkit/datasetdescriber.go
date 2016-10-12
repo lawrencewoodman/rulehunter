1fb1b4a5a39528df26b3e254ac353aea2d398852
Lawrence Woodman <lwoodman@vlifesystems.com>
2016-10-12 07:46:01 +0100

    Update rhkit to commit: 8CdC0E1

--- vendor/github.com/vlifesystems/rhkit/datasetdescriber.go
+++ vendor/github.com/vlifesystems/rhkit/datasetdescriber.go
@@ -1,19 +1,19 @@
 /*
 	Copyright (C) 2016 vLife Systems Ltd <http://vlifesystems.com>
-	This file is part of Rulehunter.
+	This file is part of rhkit.
 
-	Rulehunter is free software: you can redistribute it and/or modify
+	rhkit is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
 	the Free Software Foundation, either version 3 of the License, or
 	(at your option) any later version.
 
-	Rulehunter is distributed in the hope that it will be useful,
+	rhkit is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 	GNU General Public License for more details.
 
 	You should have received a copy of the GNU General Public License
-	along with Rulehunter; see the file COPYING.  If not, see
+	along with rhkit; see the file COPYING.  If not, see
 	<http://www.gnu.org/licenses/>.
 */
 
