a7d0f5b25a7ab8dd7bf3bdf4e35a12e81483adc9
Lawrence Woodman <lwoodman@vlifesystems.com>
2017-06-30 09:45:02 +0100

    Fix html.Run test to remove initial cmd.All files

--- html/html_test.go
+++ html/html_test.go
@@ -62,8 +62,8 @@ func TestRun_quit(t *testing.T) {
 	}
 }
 
-// Tests Run for cmd.Reports
-func TestRun_cmd_reports(t *testing.T) {
+// Tests Run for cmd.All
+func TestRun_cmd_all(t *testing.T) {
 	quit := quitter.New()
 	l := testhelpers.NewLogger()
 	htmlCmds := make(chan cmd.Cmd)
@@ -128,29 +128,132 @@ func TestRun_cmd_reports(t *testing.T) {
 			"index.html"),
 	}
 
-	hasQuitC := make(chan bool)
-	go func() {
-		Run(config, pm, l, quit, htmlCmds)
-		hasQuitC <- true
-	}()
+	go Run(config, pm, l, quit, htmlCmds)
+	defer quit.Quit()
 
-	reportsC := time.NewTimer(time.Second).C
+	var allC <-chan time.Time
 	timeoutC := time.NewTimer(5 * time.Second).C
 	ticker := time.NewTicker(time.Millisecond * 100).C
+	filesRemoved := false
+	for {
+		select {
+		case <-allC:
+			htmlCmds <- cmd.All
+		case <-ticker:
+			if err := checkFilesExist(wantFiles); err == nil {
+				if !filesRemoved {
+					// Files are first removed because cmd.All is passed at start
+					if err := removeFiles(wantFiles); err != nil {
+						t.Fatalf("removeFiles: %s", err)
+					}
+					filesRemoved = true
+					allC = time.NewTimer(time.Second).C
+				} else {
+					return
+				}
+			}
+		case <-timeoutC:
+			if err := checkFilesExist(wantFiles); err != nil {
+				t.Fatalf("Run: %s", err)
+			}
+			return
+		}
+	}
+}
+
+// Tests Run for cmd.Reports
+func TestRun_cmd_reports(t *testing.T) {
+	quit := quitter.New()
+	l := testhelpers.NewLogger()
+	htmlCmds := make(chan cmd.Cmd)
+	wd, err := os.Getwd()
+	if err != nil {
+		t.Fatal("Getwd() err: ", err)
+	}
+	defer os.Chdir(wd)
+	cfgDir := testhelpers.BuildConfigDirs(t, true)
+	defer os.RemoveAll(cfgDir)
+	testhelpers.CopyFile(
+		t,
+		filepath.Join("fixtures", "reports", "bank-loss.json"),
+		filepath.Join(cfgDir, "build", "reports"),
+	)
+	testhelpers.CopyFile(
+		t,
+		filepath.Join("fixtures", "reports", "bank-profit.json"),
+		filepath.Join(cfgDir, "build", "reports"),
+	)
+	testhelpers.CopyFile(
+		t,
+		filepath.Join("fixtures", "descriptions", "bank-loss.json"),
+		filepath.Join(cfgDir, "build", "descriptions"),
+	)
+	testhelpers.CopyFile(
+		t,
+		filepath.Join("fixtures", "descriptions", "bank-profit.json"),
+		filepath.Join(cfgDir, "build", "descriptions"),
+	)
+
+	pm, err := progress.NewMonitor(
+		filepath.Join(cfgDir, "build", "progress"),
+		htmlCmds,
+	)
+	if err != nil {
+		t.Fatalf("NewMonitor() err: %v", err)
+	}
+	config := &config.Config{
+		ExperimentsDir:    filepath.Join(cfgDir, "experiments"),
+		WWWDir:            filepath.Join(cfgDir, "www"),
+		BuildDir:          filepath.Join(cfgDir, "build"),
+		MaxNumReportRules: 100,
+	}
+	wantFiles := []string{
+		filepath.Join(cfgDir, "www", "index.html"),
+		filepath.Join(cfgDir, "www", "activity", "index.html"),
+		filepath.Join(cfgDir, "www", "reports", "how-to-make-a-loss",
+			"index.html"),
+		filepath.Join(cfgDir, "www", "reports", "how-to-make-a-profit",
+			"index.html"),
+		filepath.Join(cfgDir, "www", "tag", "test",
+			"index.html"),
+		filepath.Join(cfgDir, "www", "tag", "bank",
+			"index.html"),
+		filepath.Join(cfgDir, "www", "tag", "fahrenheit-451",
+			"index.html"),
+		filepath.Join(cfgDir, "www", "tag", "fred-ned",
+			"index.html"),
+		filepath.Join(cfgDir, "www", "tag", "hot-in-the-city",
+			"index.html"),
+	}
+
+	go Run(config, pm, l, quit, htmlCmds)
+	defer quit.Quit()
+
+	var reportsC <-chan time.Time
+	timeoutC := time.NewTimer(5 * time.Second).C
+	ticker := time.NewTicker(time.Millisecond * 100).C
+	filesRemoved := false
 	for {
 		select {
 		case <-reportsC:
 			htmlCmds <- cmd.Reports
 		case <-ticker:
 			if err := checkFilesExist(wantFiles); err == nil {
-				quit.Quit()
+				if !filesRemoved {
+					// Files are first removed because cmd.All is passed at start
+					if err := removeFiles(wantFiles); err != nil {
+						t.Fatalf("removeFiles: %s", err)
+					}
+					filesRemoved = true
+					reportsC = time.NewTimer(time.Second).C
+				} else {
+					return
+				}
 			}
 		case <-timeoutC:
-			quit.Quit()
 			if err := checkFilesExist(wantFiles); err != nil {
 				t.Fatalf("Run: %s", err)
 			}
-		case <-hasQuitC:
 			return
 		}
 	}
@@ -206,29 +309,34 @@ func TestRun_cmd_progress(t *testing.T) {
 		filepath.Join(cfgDir, "www", "activity", "index.html"),
 	}
 
-	hasQuitC := make(chan bool)
-	go func() {
-		Run(config, pm, l, quit, htmlCmds)
-		hasQuitC <- true
-	}()
+	go Run(config, pm, l, quit, htmlCmds)
+	defer quit.Quit()
 
-	progressC := time.NewTimer(time.Second).C
+	var progressC <-chan time.Time
 	timeoutC := time.NewTimer(5 * time.Second).C
 	ticker := time.NewTicker(time.Millisecond * 100).C
+	filesRemoved := false
 	for {
 		select {
 		case <-progressC:
 			htmlCmds <- cmd.Progress
 		case <-ticker:
 			if err := checkFilesExist(wantFiles); err == nil {
-				quit.Quit()
+				if !filesRemoved {
+					// Files are first removed because cmd.All is passed at start
+					if err := removeFiles(wantFiles); err != nil {
+						t.Fatalf("removeFiles: %s", err)
+					}
+					filesRemoved = true
+					progressC = time.NewTimer(time.Second).C
+				} else {
+					return
+				}
 			}
 		case <-timeoutC:
-			quit.Quit()
 			if err := checkFilesExist(wantFiles); err != nil {
 				t.Fatalf("Run: %s", err)
 			}
-		case <-hasQuitC:
 			return
 		}
 	}
@@ -281,10 +389,19 @@ func TestEscapeString(t *testing.T) {
  *   Helper Functions
  ***********************************************/
 
-func checkFilesExist(wantFiles []string) error {
-	for _, wantFile := range wantFiles {
-		if _, err := os.Stat(wantFile); os.IsNotExist(err) {
-			return fmt.Errorf("file doesn't exist: %s", wantFile)
+func checkFilesExist(files []string) error {
+	for _, f := range files {
+		if _, err := os.Stat(f); os.IsNotExist(err) {
+			return fmt.Errorf("file doesn't exist: %s", f)
+		}
+	}
+	return nil
+}
+
+func removeFiles(files []string) error {
+	for _, f := range files {
+		if err := os.Remove(f); err != nil {
+			return err
 		}
 	}
 	return nil
