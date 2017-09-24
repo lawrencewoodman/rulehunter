4f460e6b719c15fcc11caf2da29723dc20a36e38
Lawrence Woodman <lwoodman@vlifesystems.com>
2017-09-24 08:02:07 +0100

    Add debug "in unix" message

--- rulehunter_unix_test.go
+++ rulehunter_unix_test.go
@@ -1,5 +1,11 @@
 // +build darwin dragonfly freebsd linux nacl netbsd openbsd solaris
 package main
 
+import "fmt"
+
 // Users that are known on a Unix system to try using
 var knownUsers = []string{"", "root"}
+
+func init() {
+	fmt.Printf("in unix\n")
+}
