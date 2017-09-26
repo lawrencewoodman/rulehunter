a179fd90889723c38ecc0075c7081ebe492c3bf0
Lawrence Woodman <lwoodman@vlifesystems.com>
2017-09-26 07:53:32 +0100

    Skip RunServe* tests for Windows

---
+++ cmd/serve_windows_test.go
@@ -0,0 +1,11 @@
+package cmd
+
+import "testing"
+
+func TestRunServe(t *testing.T) {
+	t.Skip("This test isn't implemented for Windows yet")
+}
+
+func TestRunServe_interrupt(t *testing.T) {
+	t.Skip("This test isn't implemented for Windows yet")
+}
