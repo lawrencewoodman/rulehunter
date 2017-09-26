3e22ceb69060b643b918f30ec1597a27030ed1cf
Lawrence Woodman <lwoodman@vlifesystems.com>
2017-09-26 12:26:30 +0100

    Make log entries comparison more resilient in test

--- cmd/serve_unix_test.go
+++ cmd/serve_unix_test.go
@@ -3,6 +3,7 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 	"reflect"
@@ -95,14 +96,8 @@ func TestRunServe(t *testing.T) {
 	}
 
 	gotEntries := l.GetEntries()
-	sort.Slice(gotEntries, func(i, j int) bool {
-		return gotEntries[i].Msg < gotEntries[j].Msg
-	})
-	sort.Slice(wantEntries, func(i, j int) bool {
-		return wantEntries[i].Msg < wantEntries[j].Msg
-	})
-	if !reflect.DeepEqual(gotEntries, wantEntries) {
-		t.Errorf("GetEntries() got: %v\n want: %v", gotEntries, wantEntries)
+	if err := doLogEntriesMatch(gotEntries, wantEntries); err != nil {
+		t.Errorf("GetEntries: %s", err)
 	}
 	// TODO: Test all files generated
 }
@@ -172,4 +167,47 @@ func TestRunServe_interrupt(t *testing.T) {
 	if len(gotReportFiles) != 1 {
 		t.Errorf("runServe - gotReportFiles: %v, len(want): 1", gotReportFiles)
 	}
+}
+
+/*********************************
+ *    Helper functions
+ *********************************/
+
+func uniqLogEntries(entries []testhelpers.Entry) []testhelpers.Entry {
+	encountered := map[string]bool{}
+	result := []testhelpers.Entry{}
+	for _, e := range entries {
+		id := fmt.Sprintf("%d-%s", e.Level, e.Msg)
+		if !encountered[id] {
+			encountered[id] = true
+			result = append(result, e)
+		}
+	}
+	return result
+}
+
+func doLogEntriesMatch(got, want []testhelpers.Entry) error {
+	sort.Slice(got, func(i, j int) bool {
+		return got[i].Msg < got[j].Msg
+	})
+	sort.Slice(want, func(i, j int) bool {
+		return want[i].Msg < want[j].Msg
+	})
+	uniqGot := uniqLogEntries(got)
+	for _, eG := range uniqGot {
+		found := false
+		for _, eW := range want {
+			if eG.Level == eW.Level && eG.Msg == eG.Msg {
+				found = true
+			}
+		}
+		if !found {
+			return fmt.Errorf("got: %v, want: %v", got, want)
+		}
+	}
+	if len(got)-len(uniqGot) > 2 {
+		return fmt.Errorf("too big a difference between entries got: %v\n want: %v",
+			got, want)
+	}
+	return nil
 }
