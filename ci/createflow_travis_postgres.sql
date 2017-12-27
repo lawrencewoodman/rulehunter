dc12ff957f15a0d5ca4b9e10d5873983429acecb
Lawrence Woodman <lwoodman@vlifesystems.com>
2017-12-27 08:32:18 +0000

    Correct postgres createflow script

---
+++ ci/createflow_travis_postgres.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE master;
+CREATE TABLE flow(
+  grp char(1),
+  district varchar(30),
+  height int,
+  flow float
+);
+INSERT INTO flow
+VALUES ('a','northcal',120,16.5),
+('a','midcal',128,19),
+('a','southcal',18,5),
+('b','northcal',20,19.25),
+('b','midcal',28,10),
+('b','southcal',8,7),
+('c','northcal',320,20.73),
+('c','midcal',328,82.4),
+('c','southcal',38,1);
