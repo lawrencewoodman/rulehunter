ba4b3668b5d850b48e1618ce1726e9b6e84e78fa
Lawrence Woodman <lwoodman@vlifesystems.com>
2017-12-27 11:12:30 +0000

    Remove database name from postgres test

--- experiment/experiment_travis_test.go
+++ experiment/experiment_travis_test.go
@@ -54,7 +54,7 @@ func TestMakeDataset_travis(t *testing.T) {
 			SQL: &sqlDesc{
 				DriverName:     "postgres",
 				DataSourceName: "user=postgres dbname=master",
-				Query:          "select * from \"master\".\"flow\"",
+				Query:          "select * from \"flow\"",
 			},
 		},
 			fields: []string{"grp", "district", "height", "flow"},
@@ -69,7 +69,7 @@ func TestMakeDataset_travis(t *testing.T) {
 			SQL: &sqlDesc{
 				DriverName:     "postgres",
 				DataSourceName: "user=postgres dbname=master",
-				Query:          "select grp,district,flow from \"master\".\"flow\"",
+				Query:          "select grp,district,flow from \"flow\"",
 			},
 		},
 			fields: []string{"grp", "district", "flow"},
